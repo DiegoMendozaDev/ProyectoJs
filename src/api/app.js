ccc94e177b84767385333859901cd9ba7a736df1
DiegoMendozaDev <you@example.com>
2025-01-28 19:42:37 +0100

    vercel prueba 2

--- src/api/app.js
+++ src/api/app.js
@@ -1,9 +1,12 @@
-const express = require('serveless-http');
+const express = require('express');
+const serverless = require('serverless-http'); // Para adaptar Express al entorno de Vercel
 const app = express();
 
-app.get('/',(req,res) =>{
-    res.send('¡hola mundo, desde express!');
+// Define tus rutas
+app.get('/', (req, res) => {
+    res.send('¡Hola, mundo con Express en Vercel!');
 });
 
-module.exports =app;
-module.exports.handler = serverless(app)
+// Exporta la app para que funcione como serverless
+module.exports = app;
+module.exports.handler = serverless(app);
