2a4258af3ab85f72e92ba0b3e294909de6fc49e3
DiegoMendozaDev <you@example.com>
2025-01-28 20:42:38 +0100

    storage

--- api/app.js
+++ api/app.js
@@ -1,13 +1,18 @@
 const express = require('express');
-const serverless = require('serverless-http'); // Para adaptar Express al entorno de Vercel
+const { get } = require('@vercel/edge-config');
+const serverless = require('serverless-http');
 const app = express();
 
-// Define tus rutas
-app.get('/test', (req, res) => {
-    console.log('hola');
-    res.send('¡Hola, mundo con Express en Vercel!');
+// Ruta para obtener un valor de la configuración en Edge Config Store
+app.get('/config', async (req, res) => {
+  try {
+    const apiKey = await get('API_KEY');  // Accede al valor configurado en el Edge Config Store
+    res.json({ apiKey });
+  } catch (error) {
+    res.status(500).json({ error: 'Error al obtener la configuración' });
+  }
 });
 
-// Exporta la app para que funcione como serverless
+// Exporta la app
 module.exports = app;
 module.exports.handler = serverless(app);
